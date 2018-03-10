41ba4c1c3c4bb509e47a12af4d5ea819dd01bee1
Phill Ayers <phillip.ayers@sharpnotions.com>
2018-03-10 11:37:51 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pantry-list-slack-bot
+
+## How to deploy
+
+Rename `.env.example` to `.env` and use the value for 'Bot User OAuth Access Token' from the section 'OAuth & Permissions'
+
+```
+$ now --dotenv
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+const { json } = require('micro')
+const { WebClient } = require('@slack/client');
+
+const token = process.env.SLACK_TOKEN;
+
+module.exports = async (req, res) => {
+  const data = await json(req)
+  if (data && data.type === 'url_verification') {
+    return res.end(data.challenge);
+  }
+
+  if (data.event.type === 'message' && data.event.subtype !== 'bot_message') {
+    const conversationId = data.event.channel
+    const web = new WebClient(token)
+
+    web.chat.postMessage({ channel: conversationId, text: 'It works!' })
+    .then((response) => {
+      // `res` contains information about the posted message
+      console.log('Message sent: ', response.ts);
+    })
+    .catch(console.error);
+  }
+
+  return 'Hello World';
+}
