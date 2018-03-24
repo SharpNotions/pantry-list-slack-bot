d33aaa49fe92b490f75a3c6dc3fcacde9a5b21a1
Phill Ayers <phillip.ayers@sharpnotions.com>
2018-03-24 11:50:12 -0400

    retry deploy without slack notification and NODE_ENV

--- scripts/ci/build_env.sh
+++ scripts/ci/build_env.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "create .env file"
-echo "SLACK_TOKEN=${SLACK_TOKEN}\nNODE_ENV=production\nSLACK_WEBHOOK_URL=${SLACK_WEBHOOK_URL}" &> .env
+echo "SLACK_TOKEN=${SLACK_TOKEN}\nSLACK_WEBHOOK_URL=${SLACK_WEBHOOK_URL}" &> .env
 echo ".env created"
