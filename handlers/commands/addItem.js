b80219e6d5cb8acd5fca391f5ec2f406bc6ce6a5
Earnest Chestnut Jr <earnest.chestnut@sharpnotions.com>
2018-04-26 14:39:54 -0400

    refactor: Send ephemeral responses to users of the add slash command (#8)

--- handlers/commands/addItem.js
+++ handlers/commands/addItem.js
@@ -1,56 +1,112 @@
-const parse = require("urlencoded-body-parser");
-const fetch = require("node-fetch");
+const parse = require('urlencoded-body-parser')
+const fetch = require('node-fetch')
 
-const { send } = require("micro");
+const { send } = require('micro')
+
 const parseRequestText = text => {
-  const descriptionIncluded = text.includes("|");
-  let parsedRequestText;
+  const descriptionIncluded = text.includes('|')
+  let parsedRequestText
 
   if (descriptionIncluded) {
-    splitText = text.split("|");
+    splitText = text.split('|')
     parsedRequestText = {
       item_name: splitText[0],
       item_details: { description: splitText[1] }
-    };
+    }
   } else {
     parsedRequestText = {
       item_name: text,
-      item_details: { description: "" }
-    };
+      item_details: { description: '' }
+    }
   }
-  return parsedRequestText;
-};
+  return parsedRequestText
+}
+
+const createAttachment = data => {
+  let attachment = { title: data.item_name }
+
+  if (data.item_details && data.item_details.description !== '') {
+    attachment.text = ` \n${data.item_details.description}`
+  }
+  return attachment
+}
+
+const sendResponse = (res, exp, data) => {
+  switch (exp) {
+    case 'token':
+      send(res, 200, {
+        response_type: 'ephemeral',
+        text:
+          "Sorry, that didn't work. Authorization is required to use the `/add` slash command."
+      })
+      return
+    case 'required':
+      send(res, 200, {
+        response_type: 'ephemeral',
+        text:
+          "Sorry, that didn't work. Item name is required. Usage hint: `/add Item Name|Description`."
+      })
+      return
+    case 'error':
+      send(res, 200, {
+        response_type: 'ephemeral',
+        text: 'There was a problem with your request. Please try again.'
+      })
+      return
+    default:
+      send(res, 200, {
+        response_type: 'ephemeral',
+        text: `You successfully added your item to the pantry list.`,
+        attachments: [
+          createAttachment(data),
+          {
+            title: 'Open Pantry List',
+            title_link: 'https://pantry-list-frontend.now.sh/'
+          }
+        ]
+      })
+      return
+  }
+}
+
+const validateRequest = {
+  text: (res, text) => {
+    if (text === '') {
+      sendResponse(res, 'required')
+    }
+  },
+
+  token: (res, token, slackVerificationToken) => {
+    if (token !== slackVerificationToken) {
+      sendResponse(res, 'token')
+    }
+  }
+}
 
 module.exports = async (req, res) => {
-  const { text, team_domain, token, user_name } = await parse(req);
+  try {
+    const { text, team_domain, token, user_name } = await parse(req)
 
-  if (token !== process.env.SLACK_VERIFICATION_TOKEN) {
-    send(res, 401, "Token did not equal verification token");
-    return;
+    validateRequest.token(res, token, process.env.SLACK_VERIFICATION_TOKEN)
+    validateRequest.text(res, text)
+
+    const data = parseRequestText(text)
+    const email = `${user_name}@${team_domain}.com`
+    const url = `${process.env.PANTRY_LIST_API_URL}/item?user=${email}`
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        authorization: `Basic ${process.env.SLACK_TOKEN}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+
+    response.ok
+      ? sendResponse(res, 'default', data)
+      : sendResponse(res, 'error')
+  } catch (error) {
+    console.log(error)
   }
-
-  if (text === "") {
-    send(res, 200, {
-      response_type: "ephemeral",
-      text:
-        "Sorry, that didn't work. Item name is required. (usage hint: /add Item Name|Description)"
-    });
-    return;
-  }
-
-  const data = parseRequestText(text);
-  const email = `${user_name}@${team_domain}.com`;
-  const url = `${process.env.PANTRY_LIST_API_URL}/item?user=${email}`;
-
-  fetch(url, {
-    method: "POST",
-    headers: {
-      authorization: `Basic ${process.env.SLACK_TOKEN}`,
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  })
-    .then(res => res.text())
-    .then(body => console.log(body))
-    .catch(console.error);
-};
+}
