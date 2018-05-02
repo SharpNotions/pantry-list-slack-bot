9797ca8d5bc0b8d07b9c3dabeb1eb00a88c50dfd
Earnest Chestnut Jr <earnest.chestnut@sharpnotions.com>
2018-05-02 14:38:54 -0400

    feat: Get pantry list items via slash command (#7)

---
+++ handlers/commands/getItems.js
@@ -0,0 +1,82 @@
+const parse = require('urlencoded-body-parser')
+const fetch = require('node-fetch')
+
+const { send } = require('micro')
+
+const createAttachment = data => {
+  let attachment = {
+    color: '#7c4be0',
+    title: data.item_name
+  }
+
+  if (data.item_details && data.item_details.description !== '') {
+    attachment.text = ` \n${data.item_details.description}`
+  }
+
+  return attachment
+}
+
+const formatDefaultResponse = items => {
+  return items.length > 0
+    ? {
+        text: `*${items.length} items found*:`,
+        attachments: items.map(data => createAttachment(data))
+      }
+    : {
+        text: `*No items found*.`
+      }
+}
+
+const sendResponse = (res, exp, items = []) => {
+  switch (exp) {
+    case 'token':
+      send(res, 200, {
+        response_type: 'ephemeral',
+        text:
+          "Sorry, that didn't work. Authorization is required to use the `/list` slash command."
+      })
+      return
+    case 'error':
+      send(res, 200, {
+        response_type: 'ephemeral',
+        text: 'There was a problem with your request. Please try again.'
+      })
+      return
+    default:
+      send(res, 200, formatDefaultResponse(items))
+      return
+  }
+}
+
+const validateRequest = {
+  token: (res, token, slackVerificationToken) => {
+    if (token !== slackVerificationToken) {
+      sendResponse(res, 'token')
+    }
+  }
+}
+
+module.exports = async (req, res) => {
+  try {
+    const { team_domain, token, user_name } = await parse(req)
+
+    validateRequest.token(res, token, process.env.SLACK_VERIFICATION_TOKEN)
+
+    const email = `${user_name}@${team_domain}.com`
+    const url = `${process.env.PANTRY_LIST_API_URL}/items?user=${email}`
+    const response = await fetch(url, {
+      headers: {
+        authorization: `Basic ${process.env.SLACK_TOKEN}`,
+        'Content-Type': 'application/json'
+      }
+    })
+    const items = await response.json()
+
+    response.ok
+      ? sendResponse(res, 'default', items)
+      : sendResponse(res, 'error')
+  } catch (error) {
+    console.log(error)
+    sendResponse(res, 'error')
+  }
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const { router, post } = require('microrouter')
 const { commands, notifications, fulfillment } = require('./handlers')
 
 module.exports = router(
+  post('/get-items', commands.getItems),
   post('/add-item', commands.addItem),
   post('/item-added', notifications.itemAdded),
   post('/events', fulfillment.events),
